a426164f7e24d9b0df4a92d28cca08a80dcfa68f
arminapr <arminaparva@gmail.com>
2025-11-26 22:35:01 -0500

    Update CF

--- collaborative_filtering/collaborative_filtering.R
+++ collaborative_filtering/collaborative_filtering.R
@@ -41,10 +41,6 @@ build_similarity_matrix <- function(votes_mat, similarity) {
   return (sim_scaled)
 }
 
-# ***************************
-# DO WE NEED TO CENTER DATA?? AND SHOULD WE CHANGE VOTES PARAM
-# ***************************
-
 # does the collaborative filtering to return a prediction for how a user will vote on a bill
 # parameters:
 # - votes_df: the dataframe of previous votes 
@@ -52,7 +48,7 @@ build_similarity_matrix <- function(votes_mat, similarity) {
 # - target_bill: the bill we are trying to predict the user's vote for
 # - similarity: the similarity metric
 # - k: number for top k similar users
-user_collab_filter <- function(votes_df, target_user, target_bill, similarity = 'cosine', k = 5) {
+user_collab_filter <- function(votes_df, target_user, target_bill, similarity, k) {
   if (!(target_user %in% rownames(votes_df))) {
     stop('user does not exist or has not voted on enough legislations')
   }
@@ -66,36 +62,38 @@ user_collab_filter <- function(votes_df, target_user, target_bill, similarity = 
   
   # only account for users who voted on this bill
   bill_votes <- votes_df[, target_bill]
+  
+  # need to rename the bill columns by user id, otherwise the indexing won't work
+  names(bill_votes) <- rownames(votes_df) 
+  
+  # find users that votes on this bill and remove target_user
   valid_users <- which(!is.na(bill_votes))
   valid_users <- valid_users[valid_users != target_user]
-
+  
   # mark self-similarity as NA to avoid accounting for it
   # similarities for target user
   user_similarities <- sim_matrix[target_user, ]
   user_similarities[target_user] <- NA
   user_similarities <- user_similarities[valid_users]
-  sorted_user_sim <- sort(user_similarities, decreasing=TRUE)
-  
+
   # top-k neighbors
-  sorted_users <- sort(user_similarities, decreasing = TRUE)
-  top_k_users <- sorted_users[1:k]
+  sorted_user_sim <- sort(user_similarities, decreasing = TRUE)
+  top_k_user_sim <- sorted_user_sim[1:k]
   
   # top-k votes
   bill_votes <- bill_votes[valid_users]
-  top_k_votes <- bill_votes[top_k_users]
-  print(top_k_votes)
-  
-  # weighted prediction
-  prediction <- sum(top_k_users * top_k_votes, na.rm = TRUE) /
-    sum(abs(top_k_users), na.rm = TRUE)
+
+  top_k_votes <- bill_votes[names(top_k_user_sim)]
+
+  # weighted prediction (should not have any NA at this point, but just in case we do)
+  prediction <- sum(top_k_user_sim * top_k_votes, na.rm = TRUE) /
+    sum(abs(top_k_user_sim), na.rm = TRUE)
   return(prediction)
 }
 
-house_votes <- read.csv("house_cf_118.csv", row.names = 1)
-senate_votes <- read.csv("senate_cf_118.csv", row.names = 1)
+house_votes <- read.csv("house_cf_118.csv", row.names = 1, check.names = FALSE)
+senate_votes <- read.csv("senate_cf_118.csv", row.names = 1, check.names = FALSE)
 
-house_cosine_sim <- build_similarity_matrix(t(house_votes), 'cosine')
-senate_cosine_sim <- build_similarity_matrix(t(senate_votes), 'cosine')
-
-house_l2_sim <- build_similarity_matrix(t(house_votes), 'L2')
-senate_l2_sim <- build_similarity_matrix(t(senate_votes), 'L2')
+# example use for user with icpsr id 14854 and bill 118
+# user_collab_filter(house_votes, '14854','118', 'cosine', 200)
+# user_collab_filter(senate_votes, '15021','212', 'L2', 20)
